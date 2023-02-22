46544b8acb611bc286b4703b4382ded393373909
Damian Contreras orellana <damian.contreras036@gmail.com>
2023-02-22 03:23:41 -0300

    Adding templates to the project

--- foro_platzi/foros/views.py
+++ foro_platzi/foros/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import PostModel
+from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
+class IndexView(generic.ListView):
+    # model = PostModel
+    template_name = "foros/index.html"
+    context_object_name = "latest_posts"
+    
+    def get_queryset(self):
+        return PostModel.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:10]
