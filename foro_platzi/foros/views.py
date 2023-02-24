8bc25fa9ec36041ced80570ac5c03b0117ee97d7
Damian Contreras orellana <damian.contreras036@gmail.com>
2023-02-24 20:54:27 -0300

    Comment are now functional

--- foro_platzi/foros/views.py
+++ foro_platzi/foros/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic.edit import FormMixin
-from django.views import generic
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic.edit import FormView
+from django.views.generic.detail import SingleObjectMixin
+from django.views import generic, View
 from django.utils import timezone
+from django.http import Http404, HttpResponseForbidden
 from django.urls import reverse
 
-from .models import PostModel
+from .models import PostModel,CommentModel
 from .forms import CommentForm
 
 # Create your views here.
@@ -16,12 +18,46 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return PostModel.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:10]
 
-class ForumView(generic.DetailView):
+class ForumGetView(generic.DetailView):
     model = PostModel
     template_name = 'foros/forum.html'
     
     def get_context_data(self, **kwargs):
-        context = super(ForumView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['form'] = CommentForm()
         return context
+
+class CommentPostView(SingleObjectMixin,FormView):
+    template_name = 'foros/forum.html'
+    form_class = CommentForm
+    model = CommentModel
+    success_url = '#'
+    success_message = "Your Comment has been submitted"
     
+    def post(self, request, *args, **kwargs):
+        """
+        Posts the comment only if the user is logged in.
+        """
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+        # self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+    
+    # def get_success_url(self):
+    #     return reverse('foros:Foro_detalle', kwargs={'pk': self.object.pk})
+    
+class ForumView(View):
+
+    def get(self, request, *args, **kwargs):
+        view = ForumGetView.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = CommentPostView.as_view()
+        form = CommentForm(request.POST)
+        form.instance.user = request.user
+        form.instance.post = PostModel.objects.get(id=self.kwargs['pk'])
+        if form.is_valid():
+            form.save()
+        # return redirect(reverse('foros:Foro_detalle',kwargs={'pk':self.kwargs['pk']}))
+        return view(request,*args, **kwargs)
