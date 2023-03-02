6e75a727f835e7c0b57bd57d0b9ac55c0548c0ac
Damian Contreras orellana <damian.contreras036@gmail.com>
2023-03-02 03:24:23 -0300

    Added more tests for no comments, authentication, and solved bug on no forums

--- foro_platzi/foros/tests.py
+++ foro_platzi/foros/tests.py
@@ -3,8 +3,10 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 from django.urls.base import reverse
+from django.contrib.auth import get_user
 
 from .models import PostModel, CommentModel 
+from forum_profile.models import CustomUser 
 
 class PostModelTest(TestCase):
     def setUp(self):
@@ -27,10 +29,27 @@ class PostModelTest(TestCase):
     
 class ForosViewsTest(TestCase):
     
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(email='test@example.com',nickname='testuser',password='Secret1999')
+        
+    def test_login_auth(self):
+        self.assertFalse(get_user(self.client).is_authenticated)
+        self.client.login(email='test@example.com',password='Secret1999')
+        self.assertTrue(get_user(self.client).is_authenticated)
+        
     def test_no_forums(self):
+        self.client.login(email='test@example.com',password='Secret1999')
         response = self.client.get(reverse('foros:Index'))
+        
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'No posts are available')
         self.assertQuerysetEqual(response.context['latest_posts'], [])
         
+    def test_no_comments_on_forum(self):
+        forum = PostModel.objects.create(title='Titulo random', thematic='Tematica', post_text='Post random de testeo', user=self.user)
+        f_response = self.client.get(reverse('foros:Foro_detalle', kwargs={'pk':forum.id}))
+        
+        self.assertEqual(f_response.status_code, 200)
+        self.assertContains(f_response, 'There  are no comments yet!')
     
+    # def test
