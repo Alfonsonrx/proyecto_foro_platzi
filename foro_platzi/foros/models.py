4d6f4f8a0ed982189f12f07870992a7138a6cd27
Damian Contreras orellana <damian.contreras036@gmail.com>
2023-03-02 02:21:44 -0300

    Added firsts tests, changing datefield's models to datetimefields

--- foro_platzi/foros/models.py
+++ foro_platzi/foros/models.py
@@ -1,6 +1,5 @@
 import uuid
 import datetime
-import pytz
 
 from django.db import models
 from forum_profile.models import CustomUser
@@ -13,8 +12,8 @@ class PostModel(models.Model):
     title = models.CharField(max_length=200)
     thematic = models.CharField(max_length=24, blank=True, null=True)
     post_text = models.TextField(max_length=1000, default=None)
-    pub_date = models.DateField(auto_now_add=True)
-    mod_date = models.DateField(auto_now_add=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    mod_date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=False, null=False)
     
     def __str__(self) -> str:
@@ -28,9 +27,7 @@ class PostModel(models.Model):
     )
     def was_published_recently(self):
         now = timezone.now()
-        pub = self.pub_date
-        datetime_pub = datetime.datetime(pub.year, pub.month, pub.day, 0, 0, tzinfo=pytz.utc)
-        return now - datetime.timedelta(days=1) <= datetime_pub <= now
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     
     # ...
     @admin.display(
@@ -46,9 +43,13 @@ class CommentModel(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     post = models.ForeignKey(PostModel, on_delete=models.CASCADE)
     original_comment = models.ForeignKey("CommentModel", on_delete=models.CASCADE, blank=True, null=True)
-    pub_date = models.DateField(auto_now_add=True)
-    mod_date = models.DateField(auto_now_add=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    mod_date = models.DateTimeField(auto_now_add=True)
     text_comment = models.TextField(max_length=500)
     
     def __str__(self) -> str:
         return self.text_comment[:30]
+    
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
--- foro_platzi/foros/tests.py
+++ foro_platzi/foros/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+from django.urls.base import reverse
+
+from .models import PostModel, CommentModel 
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        self.post = PostModel(title='Titulo random', thematic='Tematica', post_text='Post random de testeo')
+    
+    def test_recently_published_future(self):
+        time = timezone.now() + datetime.timedelta(days=15)
+        self.post.pub_date = time
+        self.assertFalse(self.post.was_published_recently())
+    
+    def test_recently_published_past(self):
+        time = timezone.now() + datetime.timedelta(days=-15)
+        self.post.pub_date = time
+        self.assertFalse(self.post.was_published_recently())
+    
+    def test_recently_published_present(self):
+        time = timezone.now()
+        self.post.pub_date = time
+        self.assertTrue(self.post.was_published_recently())
+    
+class ForosViewsTest(TestCase):
+    
+    def test_no_forums(self):
+        response = self.client.get(reverse('foros:Index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No posts are available')
+        self.assertQuerysetEqual(response.context['latest_posts'], [])
+        
+    
